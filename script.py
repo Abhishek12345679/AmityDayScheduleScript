02666c7aa22889f2f60012c448008277eb5b155b
Abhishek Sah <sah755146@gmail.com>
2022-02-10 00:55:59 +0530

    using data as json from the schedule instead of a screenshot

--- script.py
+++ script.py
@@ -2,7 +2,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -10,15 +9,37 @@ from email.mime.multipart import MIMEMultipart
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 from webdriver_manager.utils import ChromeType
+from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
+# login Function
+def login():
+    print('~~Amity Scheduler~~')
+    print('\n')
+    print('started!!')
 
-def sendMail(image, browser):
+    site = "https://s.amizone.net/"
+    browser.get(site)
 
-    # replace os.environ.get with your Amity Email Address
-    # replace os.environ.get with your Amity Email Password
-    EMAIL = os.environ.get('EMAIL')
-    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    browser.implicitly_wait(5)
 
+    print('Opened Amizone ...')
+    print(f'Entering Login Credentials!')
+
+    print(f'ENROLLMENT_NO: {ENROLLMENT_NO}')
+    print(f'Password: {AMITY_PASSWORD}')
+
+    browser.find_element_by_name(
+        '_UserName').send_keys(ENROLLMENT_NO)
+
+    browser.find_element_by_name('_Password').send_keys(AMITY_PASSWORD)
+
+    submit_btn = browser.find_element_by_class_name("login100-form-btn")
+    submit_btn.send_keys(Keys.ENTER)
+
+    
+def sendMail(image):
     print(f'Email: {EMAIL}')
     print(f'Password: {EMAIL_PASSWORD}')
     print(f"Sending Email to {EMAIL}")
@@ -56,63 +77,49 @@ def sendMail(image, browser):
     browser.close()
 
 
-def scrapeSite():
+def getDaySchedule():
+    
+    # clicking on home li
+    WebDriverWait(browser, 20)\
+        .until(EC.element_to_be_clickable((By.CLASS_NAME,"nav-list > li:first-child > a")))\
+        .click()
+        
+    # closing the students welfare department modal   
+    browser.implicitly_wait(20)
+    modal_close_btn = browser.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[2]/div/div/div[1]/button")
+    modal_close_btn.click()
+    
+    schedule_list = browser\
+                .find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[2]/div/div/div/div[7]/div[1]/div/div/div/div[2]/div/div[2]/div/div/table")\
+                .text\
+                .split('\n')
+    
+    schedule_list.pop(0)
+    
+    # print(f'{schedule_list}')
 
+
+if __name__ == "__main__":
+    
     # replace os.environ.get with your enrollment number
     ENROLLMENT_NO = os.getenv('ENROLLMENT_NO')
     # replace os.environ.get with your enrollment password
     AMITY_PASSWORD = os.getenv('AMITY_PASSWORD')
-
-    print('~~Amity Scheduler~~')
-    print('\n')
-    print('started!!')
-
-    site = "https://student.amizone.net"
-
+    
+    # replace os.environ.get with your Amity Email Address
+    EMAIL = os.environ.get('EMAIL')
+    # replace os.environ.get with your Amity Email Password
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=2560,1600")
 
     browser = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(),
                                options=chrome_options)
-    browser.get(site)
-
-    time.sleep(5)
-
-    print('Opened Amizone ...')
-    print(f'Entering Login Credentials!')
-
-    print(f'ENROLLMENT_NO: {ENROLLMENT_NO}')
-    print(f'Password: {AMITY_PASSWORD}')
-
-    browser.find_element_by_name(
-        '_UserName').send_keys(ENROLLMENT_NO)
-
-    browser.find_element_by_name('_Password').send_keys(AMITY_PASSWORD)
-
-    submit_btn = browser.find_element_by_class_name("login100-form-btn")
-    submit_btn.send_keys(Keys.ENTER)
-
-    time.sleep(20)
-
-    # close modals
-    close_btn = browser.find_element_by_css_selector(
-        '#MyPopup15 > div > div > div.modal-header > button')
-    close_btn.send_keys(Keys.ENTER)
-
-    currentdatetime = str(datetime.now())
-    print(currentdatetime)
-    image_name = currentdatetime[0:currentdatetime.rindex(
-        ':', 0, len(currentdatetime))]
-    print(image_name)
-
-    time.sleep(20)
-
-    image = browser.find_element_by_css_selector(
-        '#Div_Partial > div.main-content > div > div.page-content > div > div > div > div:nth-child(7) > div:nth-child(1)').screenshot_as_png
-    sendMail(image, browser)
-
-
-scrapeSite()
+    login()
+    schedule = getDaySchedule()
+    # sendMail(schedule)
+    
