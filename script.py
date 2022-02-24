d9a7549baa4ccbd9dac0e8f646986f1f51e00c73
Abhishek12345679 <sah755146@gmail.com>
2022-02-24 16:53:27 +0530

    changed timing

--- script.py
+++ script.py
@@ -150,14 +150,14 @@ if __name__ == "__main__":
 
     sched = BlockingScheduler()
 
-    @sched.scheduled_job('interval', minutes=5)
-    def timed_job():
-        print('This job is run every five minutes.')
-        main(browser)
-
-    # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
-    # def scheduled_job():
-    #     print('This job is run every weekday at 0730.')
+    # @sched.scheduled_job('interval', minutes=5)
+    # def timed_job():
+    #     print('This job is run every five minutes.')
     #     main(browser)
+
+    @sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
+    def scheduled_job():
+        print('This job is run every weekday at 7 am.')
+        main(browser)
 
     sched.start()
