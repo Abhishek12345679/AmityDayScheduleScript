033a3d4aa9a05ffae76afcd49558545bc56b6e05
Abhishek12345679 <sah755146@gmail.com>
2022-02-24 14:05:25 +0530

    add job for testing

--- clock.py
+++ clock.py
@@ -7,6 +7,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=5)
 def timed_job():
     print('This job is run every five minutes.')
+    main()
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=7, minutes=30)
